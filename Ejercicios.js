9e2b1f3943502ccf2e26bc9eb7d0d04906d5a8ce
TheWonker <114485695+ManuelAlvare@users.noreply.github.com>
2023-01-16 11:13:43 +0100

    Create Ejercicios.js

---
+++ Ejercicios.js
@@ -0,0 +1,35 @@
+    // Crea una lista de números pares del 2 al 20 utilizando un bucle while.
+    var numeros =[];
+    var i=0;
+    while(i<=20){
+numeros.push(i);
+i= i + 2;
+    }
+console.log("Listas de numeros"+numeros)
+
+    // Crea una lista de números impares del 1 al 19 utilizando un bucle for.
+    var impares=[];
+for (i=1; i<=19; i++){
+if (i % 2==1){
+    impares.push(i);
+}
+}
+console.log("Lista de numeros"+numeros)
+    // Crea una lista de comida.
+    var comida = ["pasta", "hamburguesa","durum"]
+    
+    // Agrega un elemento al principio de la lista de comida que creaste en el ejercicio anterior.
+    
+    // Accede al segundo elemento de la lista de comida y cambia su valor por otro diferente.
+    
+    // Recorre la lista de comida utilizando un bucle while y muestra cada elemento en la consola.
+    
+    // Crea una función que reciba una lista como parámetro y devuelva el último elemento de la lista.
+    
+    // Crea una función que reciba una lista y un elemento como parámetros y agregue el elemento a la lista si no se encuentra en ella.
+    
+    // Crea una función que reciba una lista como parámetro y devuelva una nueva lista con los elementos de la lista original en orden inverso.
+    
+    // Crea una función que reciba una lista de números como parámetro y devuelva la suma de todos los números de la lista.
+    
+    // Crea una función que reciba una lista de números como parámetro y devuelva la media de todos los números de la lista.
